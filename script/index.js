01e26ee1400fa074cb589e317cd1ddfe397bc629
guetzan <orcsdzn@gmail.com>
2023-11-17 23:03:00 -0300

    implementando lógica de conversão arábicos romanos

--- script/index.js
+++ script/index.js
@@ -1,25 +1,61 @@
-class ControleRemoto {
-    constructor(tv) {
-        this.tv = tv;
-        this.volume = 0;
+class conversorRomanos {
+    constructor() {
+        this.input = document.querySelector('#valorInput');
+        this.areaResultado = document.querySelector('#result');
+        this.algarismosRomanos = {
+            1000: 'M',
+             900: 'CM',
+             500: 'D',
+             400: 'CD',
+             100: 'C',
+              90: 'XC',
+              50: 'L',
+              40: 'XL',
+              10: 'X',
+               9: 'IX',
+               5: 'V',
+               4: 'IV',
+               3: 'III',
+               2: 'II',
+               1: 'I',
+        }
+        this.romanosEmIndo = (Object.keys(this.algarismosRomanos)).reverse();
     }
 
-    get teste() {
-        return this.volume;
+    conversorIndoRomano(valor) {
+        if (valor > 3999 || valor < 0) return;
+
+        valor = Number(valor);
+        let resultado = '';
+
+        for(let valorEmIndo of this.romanosEmIndo) {
+            while(valor >= valorEmIndo) {
+                valor -= valorEmIndo;
+                resultado += this.algarismosRomanos[valorEmIndo];
+            }
+        }
+
+        return resultado;
     }
 
-    aumentarVolume() {
-        this.volume += 1;
-    }
-    
-    diminuirVolume() {
-        this.volume -= 1;
-    }
+    atualizarResultado() {
+        const valorAtual = this.conversorIndoRomano(this.input.value);
+        console.log(valorAtual)
 
-    static trocaPilha() {
-        console.log('pilha')
+        if(typeof valorAtual === 'undefined') {
+            this.areaResultado.style.fontFamily = "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif";
+            this.areaResultado.style.fontSize = '1.8em';
+            this.areaResultado.innerHTML = 'Valor inválido. Utilize valores entre 0 e 3999.'; 
+            return;
+        }
+
+        this.areaResultado.innerHTML = valorAtual;
     }
 }
 
-const controle = new ControleRemoto('LG');
-ControleRemoto.trocaPilha();
+conversor = new conversorRomanos
+
+conversor.input.addEventListener('keyup', () => {
+    conversor.conversorIndoRomano();
+    conversor.atualizarResultado();
+})
