3479e51c8131dd637f45ac93a3960bd1e01a8a9b
guetzan <orcsdzn@gmail.com>
2023-11-18 22:33:46 -0300

    subindo progresso do verificador de subtraidos

--- script/index.js
+++ script/index.js
@@ -1,8 +1,8 @@
-class conversorRomanos {
+class ConversorRomanos {
 	constructor() {
 		this.input = document.querySelector('#valorInput');
 		this.areaResultado = document.querySelector('#result');
-		this.modoAtual = 'arabicoRomano';
+		this.modoAtual = 'romanoArabico';
 		this.botoesOpcao = document.querySelectorAll('.options button');
 		this.algarismosRomanos   = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 		this.valoresIndoArabicos = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
@@ -57,7 +57,7 @@ class conversorRomanos {
 		
 		let resultado = '';
 		
-		for(let index = 0; index <= this.valoresIndoArabicos.length; index++) {
+		for(let index = 0; index < this.valoresIndoArabicos.length; index++) {
 			while(valor >= this.valoresIndoArabicos[index]) {
 				valor -= this.valoresIndoArabicos[index];
 				resultado += this.algarismosRomanos[index];
@@ -79,10 +79,10 @@ class conversorRomanos {
 				continue;
 			}
 			
-			if(valor[caractere] == ultimoCaractere) contadorCaractere++;
+			if(valor[caractere] === ultimoCaractere) contadorCaractere++;
 		}
 		
-
+		
 		if(contadorCaractere > 3) return true;
 		return false;
 	}
@@ -94,23 +94,22 @@ class conversorRomanos {
 		for(let caractere of valor) {
 			if(!this.algarismosRomanos.includes(caractere)) return;
 		}
-
+		
 		if(this.excedeLimite(valor)) return;
-
+		if(this.subtracaoInvalida()) return;
+		
 		for(let caractere = 0; caractere < valor.length; caractere++) {	
 			for(let i = 0; i < this.algarismosRomanos.length; i++) {
 				const indexProximoCaractere = caractere + 1;
 				const caracteresAdjacentes = valor[caractere] + valor[indexProximoCaractere];
-
+				
 				if(caracteresAdjacentes === this.algarismosRomanos[i]) {
 					soma += this.valoresIndoArabicos[i];
 					delete valor[caractere]; delete valor[indexProximoCaractere];
 				}
 				
 				if((valor[caractere]) === this.algarismosRomanos[i]) {
-					if(valor[caractere] > valor[indexProximoCaractere]) {
-						return;
-					}
+					
 					soma += this.valoresIndoArabicos[i];
 				}
 			}
@@ -118,6 +117,102 @@ class conversorRomanos {
 		
 		return soma;
 	}
+	
+	subtracaoInvalida() {
+		let valor = 'CMIX';
+		
+		for(let index = 0; index < valor.length; index++) {
+			let enderecoCaractere;
+			let valorVizinho;
+			let enderecoCaractereVizinho;
+
+			if(valor[index + 1]) {
+				const valorAdjacente = valor[index] + valor[index + 1];
+				let valorVizinho;
+				let valorAdjacenteVizinho;
+				let caractereSubtraido = false;
+
+				if(this.algarismosRomanos.includes(valorAdjacente)) {
+					enderecoCaractere = this.algarismosRomanos.indexOf(valorAdjacente);	
+					caractereSubtraido = true;
+				} 
+
+				if(caractereSubtraido === true) {
+					if(valor[index+2]) valorVizinho = valor[index+2];
+					if(valor[index+3]) valorVizinho += valor[index+3];
+
+					if(this.algarismosRomanos.includes(valorVizinho)) {
+						enderecoCaractereVizinho = this.algarismosRomanos.indexOf(valorVizinho)
+						
+						if(this.algarismosRomanos(enderecoCaractere))
+					}
+
+					// if(valor[index+2]) {
+					// 	if(valor[index+3]) {
+	
+					// 		if(this.algarismosRomanos.includes(valorAdjacenteVizinho)) {
+					// 			enderecoCaractereVizinho = this.algarismosRomanos.indexOf(valorAdjacenteVizinho) 
+							
+					// 			if(this.valoresIndoArabicos[enderecoCaractere] < this.valoresIndoArabicos[enderecoCaractereVizinho]) {
+					// 				return;
+					// 			}
+					// 		}
+					// 	}
+
+					// 	console.log(valor[index+3]);
+
+					// 	if(valor[index+2]) {
+					// 		console.log('entrou')
+					// 		valorVizinho = valor[index+2];
+					// 		console.log(valorVizinho);
+					// 	} 
+					// }
+
+				}
+				
+				// if(valor[index+2]) {
+				// 	if(caractereSubtraido) {
+				// 		caractereVizinho = valor[index+2];
+				// 		console.log(caractereVizinho);
+				// 	}
+				// }
+				
+				// if(valor[index+2]) {
+				// 	const valorAdjacenteVizinho = valor[index+1] + valor[index+2]; 
+					
+				// 	if(this.algarismosRomanos.includes(valorAdjacenteVizinho)) {
+				// 		const enderecoCaractereVizinho = this.algarismosRomanos.indexOf(valorAdjacenteVizinho);
+				// 	}
+				// }
+			}
+			
+			
+			
+			// if(caractereVizinho) {
+			// 	//caractere menor antecedendo valor subtraído maior
+			// 	if(valor[index+2]) {
+			// 		const valorAdjacenteVizinho = valor[index+1] + valor[index+2]; 
+			
+			// 		if(this.algarismosRomanos.includes(valorAdjacenteVizinho)) {
+			// 			const enderecoValorAdjacente = this.algarismosRomanos.indexOf(valorAdjacenteVizinho);
+			
+			// 			if(this.valoresIndoArabicos[enderecoCaractere] < this.valoresIndoArabicos[enderecoValorAdjacente]) {
+			// 				return true;
+			// 			}
+			
+			// 			return false;
+			// 		}
+			// 	}
+			
+			// 	//caractere menor antecedendo caractere maior
+			// 	if(this.valoresIndoArabicos[enderecoCaractere] < this.valoresIndoArabicos[enderecoCaractereVizinho]) {
+			// 		// console.log(valor[index-1],valor[index], caractereVizinho)
+			// 		return true;
+			// 	}
+			// }
+		}		
+	}
+	
 	
 	atualizarResultado(resultadoAtual) {
 		const valorAtual = resultadoAtual;
@@ -133,6 +228,6 @@ class conversorRomanos {
 	}
 }
 
-conversor = new conversorRomanos;
+conversor = new ConversorRomanos;
 conversor.capturarTrocaOpcao();
 conversor.capturarTeclado();
